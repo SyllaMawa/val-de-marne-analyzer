cb4a82bf81f553bb1438417473927ca3a587151a
SyllaMawa <sylla.mawa2001@gmail.com>
2026-09-21 22:56:41 +0200

    Prepare backend for Render deployment

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 
-DATABASE_URL = (
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
     "postgresql+psycopg2://postgres:postgres@localhost:5433/valdemarne"
 )
 
